274d3b05159523cd9c351a469fa122d0bfa3ce59
lks <luks90nunes@gmail.com>
2026-03-01 19:32:30 -0300

    chapter11

---
+++ ch11/log3/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"errors"
+)
+
+func main() {
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+	log.Println("Start of our app")
+	err := errors.New("Application Aborted!")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("End of our app")
+}
